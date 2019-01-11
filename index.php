524ee7bd5e3a06e10452484a1405c74e080a7725
= <=>
2019-01-11 12:07:50 +0100

    Adding partials

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include('partials/_header.php'); ?>
+
+<!--Contenu de index.php, board.php,login, singnup-->
+
+
+<?php include('partials/_footer.php'); ?>
+
+
+
+
+
+
+
+
+
---
+++ views/board.php
@@ -0,0 +1,6 @@
+<?php include('../partials/_header.php'); ?>
+
+<!--Contenu de index.php, board.php,login, singnup-->
+
+
+<?php include('../partials/_footer.php'); ?>
